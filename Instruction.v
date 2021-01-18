6a47c702287b19eec31311ad8ad2f2e5b8cce2d3
Jonas Fiege <jonas.fiege@gmail.com>
2021-01-18 11:38:26 +0100

    Add lemmas to module Instruction

--- Instruction.v
+++ Instruction.v
@@ -2,18 +2,49 @@ Require Import Coq.Structures.Equalities.
 Require Import Coq.Classes.RelationPairs.
 
 Module Instruction (Opcode Operand : DecidableType) <: DecidableType.
-  Definition t : Type := Opcode.t * Operand.t.
+  Definition t : Type :=
+    Opcode.t * Operand.t.
 
-  Definition opcode (self : t) : Opcode.t := fst self.
-  Definition operand (self : t) : Operand.t := snd self.
+  Definition opcode (self : t) : Opcode.t :=
+    fst self.
+  Definition operand (self : t) : Operand.t :=
+    snd self.
 
   Definition eq (self other : t) : Prop :=
     Opcode.eq (opcode self) (opcode other) /\ Operand.eq (operand self) (operand other).
 
-  Instance eq_equiv : Equivalence eq := RelProd_Equivalence _ _ _ _.
+  Instance eq_equiv : Equivalence eq :=
+    RelProd_Equivalence _ _ _ _.
 
   Lemma eq_dec (self other : t) : {eq self other} + {~ eq self other}.
   Proof.
-    destruct (Opcode.eq_dec (opcode self) (opcode other)) as [opcode_eq| opcode_neq]; [destruct (Operand.eq_dec (operand self) (operand other)) as [operand_eq| operand_neq]|]; firstorder.
+    destruct (Opcode.eq_dec (opcode self) (opcode other)) as [opcode_eq| opcode_neq];
+    [destruct (Operand.eq_dec (operand self) (operand other)) as [operand_eq| operand_neq]|];
+    firstorder.
   Qed.
+
+  Lemma neq_iff (self other : t) : ~ eq self other <-> ~ Opcode.eq (opcode self) (opcode other) \/ ~ Operand.eq (operand self) (operand other).
+  Proof.
+    firstorder using Opcode.eq_dec, Operand.eq_dec.
+  Qed.
+
+  Lemma neq_opcode (self other : t) : ~ Opcode.eq (opcode self) (opcode other) -> ~ eq self other.
+  Proof.
+    firstorder.
+  Qed.
+
+  Lemma neq_operand (self other : t) : ~ Operand.eq (operand self) (operand other) -> ~ eq self other.
+  Proof.
+    firstorder.
+  Qed.
+
+  Lemma neq_eq_opcode (self other : t) : Opcode.eq (opcode self) (opcode other) -> ~ eq self other <-> ~ Operand.eq (operand self) (operand other).
+  Proof.
+    firstorder.
+  Defined.
+
+  Lemma neq_eq_operand (self other : t) : Operand.eq (operand self) (operand other) -> ~ eq self other <-> ~ Opcode.eq (opcode self) (opcode other).
+  Proof.
+    firstorder.
+  Defined.
 End Instruction.
